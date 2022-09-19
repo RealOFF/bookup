6652ee52074fbfa10f7a94a6a2579059fa73358d
mhimpel <gimpelllll@gmail.com>
2022-09-19 19:38:00 +0400

    Update dashboard

--- packages/app/features/business-dashboard/business-dashboad.tsx
+++ packages/app/features/business-dashboard/business-dashboad.tsx
@@ -1,5 +1,15 @@
-import { Button, H3, XGroup, YGroup, YStack } from '@bookup/uikit'
+import { Button, ButtonProps, H3, Paragraph, XStack, YStack, H4 } from '@bookup/uikit'
+import { Tag, Gift, Users, PieChart, UserPlus, Send } from '@tamagui/feather-icons'
 import { useLink } from 'solito/link'
+
+const StyledButton = (props: ButtonProps) => <Button
+  {...props}
+  h='$7'
+  w='33.3%'
+  fd='column'
+  chromeless
+  color='black'
+/>
 
 export const BusinessDashboard = () => {
   const clientsLinkProps = useLink({
@@ -7,33 +17,49 @@ export const BusinessDashboard = () => {
   })
 
   return (
-    <YStack ai="center" maw="600">
-      <YStack space>
-        <H3 ta="center">Your business</H3>
-        <YGroup>
-          <XGroup>
-            <Button {...clientsLinkProps}>
-              Clients
-            </Button>
-            <Button>
-              Clients
-            </Button>
-            <Button>
-              Clients
-            </Button>
-          </XGroup>
-          <XGroup>
-            <Button>
-              Clients
-            </Button>
-            <Button>
-              Clients
-            </Button>
-            <Button>
-              Clients
-            </Button>
-          </XGroup>
-        </YGroup>
+    <YStack w='100%' ai="center">
+      <YStack px='$3' jc='space-between' w='100%' maw={600}>
+        <YStack w='100%' space='$6'>
+          <H3 ta="center">Your business</H3>
+          <YStack w='100%' dsp='flex' >
+            <XStack dsp='flex'>
+              <StyledButton {...clientsLinkProps}>
+                <Users color='black' />
+                Clients
+              </StyledButton>
+              <StyledButton >
+                <UserPlus color='black' />
+                Visits
+              </StyledButton>
+              <StyledButton>
+                <Send color='black' />
+                Newsletter
+              </StyledButton>
+            </XStack>
+            <XStack dsp='flex'>
+              <StyledButton>
+                <Tag color='black' />
+                Discounts
+              </StyledButton>
+              <StyledButton>
+                <PieChart color='black' />
+                Analystsp
+              </StyledButton>
+              <StyledButton>
+                <Gift color='black' />
+                Bonuses
+              </StyledButton>
+            </XStack>
+          </YStack>
+        </YStack>
+        <YStack mt='$12' p='$6' bg='$gray3' br='$5'>
+          <H4>
+            Everything for your business processes
+          </H4>
+          <Paragraph>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </Paragraph>
+        </YStack>
       </YStack>
     </YStack>
   )
